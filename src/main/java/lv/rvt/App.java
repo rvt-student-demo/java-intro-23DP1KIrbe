8697cd12be647ed131c4d0847c6a0cd4b9997582
23DP1KIrbe <166118411+23DP1KIrbe@users.noreply.github.com>
2024-09-10 09:39:03 +0000

    variables

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -7,7 +7,13 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("Hello World!");
+        String name = "Kristaps";
+        int age = 17;
+        boolean online = false;
+        
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(online);
     }
 
 }
