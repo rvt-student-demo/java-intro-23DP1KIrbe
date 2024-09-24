4aa8bfdbb3f72ab47d07b66254345f5b25c3ed35
23DP1KIrbe <166118411+23DP1KIrbe@users.noreply.github.com>
2024-09-24 07:58:32 +0000

    Number and sum of numbers

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -8,23 +8,24 @@ public class App
     {
         Scanner scanner = new Scanner(System.in);
         
-        System.out.println("Value of gift?:");
-        int gift = Integer.valueOf(scanner.nextLine());
-        
+         int summa = 0;
+         int count = 0;
 
-        if (gift < 5000) {
-            System.out.println("No tax!");
-        } else if (5000 <= gift && gift < 25000) {
-            System.out.println("Tax:" + (100 + (gift - 5000) * 0.08));
-        } else if (25000 <= gift && gift < 55000) {
-            System.out.println("Tax:" + (1700 + (gift - 25000) * 0.1));
-        } else if (55000 <= gift && gift < 200000) {
-            System.out.println("Tax:" + (4700 + (gift - 55000) * 0.12));
-        } else if (200000 <= gift && gift < 1000000) {
-            System.out.println("Tax:" + (22100 + (gift - 200000) * 0.15));
-        } else if (gift >= 1000000) {
-            System.out.println("Tax:" + (142100 + (gift - 1000000) * 0.17));
+        
+        while(true) {
+            System.out.println("Enter the number:");
+            int number = Integer.valueOf(scanner.nextLine());
+
+            if (number == 0) {
+                break;
+            }
+            summa += number;
+            count++;
+            
+
         }
+        System.out.println("Number of numbers:" + count);
+        System.out.println("Sum of numbers:" + summa);
         scanner.close();
     }
 
