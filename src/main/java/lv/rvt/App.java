c2230906cd9ceed6bc0b06ee5826c94bd814e767
23DP1KIrbe <166118411+23DP1KIrbe@users.noreply.github.com>
2024-09-24 06:08:59 +0000

    Gift tax

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -12,17 +12,17 @@ public class App
         int gift = Integer.valueOf(scanner.nextLine());
         
 
-        if (gift <= 5000) {
+        if (gift < 5000) {
             System.out.println("No tax!");
-        } else if (5000 < gift && gift < 25000) {
+        } else if (5000 <= gift && gift < 25000) {
             System.out.println("Tax:" + (100 + (gift - 5000) * 0.08));
-        } else if (25000 < gift && gift < 55000) {
+        } else if (25000 <= gift && gift < 55000) {
             System.out.println("Tax:" + (1700 + (gift - 25000) * 0.1));
-        } else if (55000 < gift && gift < 200000) {
+        } else if (55000 <= gift && gift < 200000) {
             System.out.println("Tax:" + (4700 + (gift - 55000) * 0.12));
-        } else if (200000 < gift && gift < 1000000) {
+        } else if (200000 <= gift && gift < 1000000) {
             System.out.println("Tax:" + (22100 + (gift - 200000) * 0.15));
-        } else if (gift > 1000000) {
+        } else if (gift >= 1000000) {
             System.out.println("Tax:" + (142100 + (gift - 1000000) * 0.17));
         }
         scanner.close();
