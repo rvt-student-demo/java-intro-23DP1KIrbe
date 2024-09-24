15bd3b75e95a490e42ece433838f0d992c2a456b
23DP1KIrbe <166118411+23DP1KIrbe@users.noreply.github.com>
2024-09-24 06:02:13 +0000

    Gift tax

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -8,17 +8,22 @@ public class App
     {
         Scanner scanner = new Scanner(System.in);
         
-        System.out.println("Give the first number:");
-        int pirmais = Integer.valueOf(scanner.nextLine());
-        System.out.println("Give the second number:");
-        int otrais = Integer.valueOf(scanner.nextLine());
+        System.out.println("Value of gift?:");
+        int gift = Integer.valueOf(scanner.nextLine());
+        
 
-        if (pirmais > otrais) {
-            System.out.println("Greater number is:" + pirmais);
-        } else if (otrais > pirmais) {
-            System.out.println("Greater number is:" + otrais);
-        } else {
-            System.out.println("The numbers are equal!");
+        if (gift <= 5000) {
+            System.out.println("No tax!");
+        } else if (5000 < gift && gift < 25000) {
+            System.out.println("Tax:" + (100 + (gift - 5000) * 0.08));
+        } else if (25000 < gift && gift < 55000) {
+            System.out.println("Tax:" + (1700 + (gift - 25000) * 0.1));
+        } else if (55000 < gift && gift < 200000) {
+            System.out.println("Tax:" + (4700 + (gift - 55000) * 0.12));
+        } else if (200000 < gift && gift < 1000000) {
+            System.out.println("Tax:" + (22100 + (gift - 200000) * 0.15));
+        } else if (gift > 1000000) {
+            System.out.println("Tax:" + (142100 + (gift - 1000000) * 0.17));
         }
         scanner.close();
     }
