ce77b2802a98953c7236d429b4d8c9a1f8ce669f
23DP1KIrbe <166118411+23DP1KIrbe@users.noreply.github.com>
2024-10-08 06:37:22 +0000

    Star sign

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -5,17 +5,42 @@ import java.util.Scanner;
 public class App 
 {
     public static void main( String[] args ) {
-        Scanner scanner = new Scanner(System.in);
+        //print stars
+        printStars(5);
+        printStars(3);
+        printStars(9);
 
-        divisibleByThreeInRange(2, 10);
-        scanner.close();
+        //print square
+
+        printSquare(4);
+
+        //print triangle
+
+        printTriangle(4);
     }
     
-    public static void divisibleByThreeInRange(int num1, int num2){
-        for(int i = num1; i<=num2; i++){
-            if(i % 3 == 0){
-                System.out.println(i);
-            }
+    public static void printStars(int number){
+        for(int i = 0; i<number; i++){
+            System.out.print("*");
         }
-    } 
+        System.out.println();
+    }
+    
+    public static void printSquare(int size){
+        for(int i = 0; i<size; i++){
+            for(int j = 0; j<size; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        } 
+    }
+    
+    public static void printTriangle(int height){
+        for(int i = 1; i<=height; i++){
+            for(int j = 0; j<i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
