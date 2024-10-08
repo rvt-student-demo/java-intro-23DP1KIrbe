c7700b07cdd138e8079b435b5e61269d453e6c4d
23DP1KIrbe <166118411+23DP1KIrbe@users.noreply.github.com>
2024-10-08 08:03:40 +0000

    Advanced astrology

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -17,6 +17,16 @@ public class App
         //print triangle
 
         printTriangle(4);
+        System.out.println();
+
+        //print spaces
+        printTriangleTwo(4);
+        System.out.println();
+
+        //print christamstree
+        printChristamsTree(4);
+        
+        printChristamsTree(10);
     }
     
     public static void printStars(int number){
@@ -42,5 +52,30 @@ public class App
             }
             System.out.println();
         }
+    }
+
+    public static void printSpaces(int number){
+        for(int i = 0; i<number; i++){
+            System.out.print(" ");
+        }
+    }
+
+    public static void printTriangleTwo(int size){
+        for(int i = 1; i<=size; i++){
+            printSpaces(size - i);
+            printStars(i);
+        }
+    }
+
+    public static void printChristamsTree(int height){
+        for(int i = 1; i<=height; i++){
+            printSpaces(height - i);
+            printStars(2 * i - 1);
+        
+        }
+        printSpaces(height-2);
+        System.out.println("***");
+        printSpaces(height-2);
+        System.out.println("***");
     }
 }
