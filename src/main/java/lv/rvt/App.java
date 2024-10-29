cee62e9397d13f95a644c53c09db7b7235a02d20
23DP1KIrbe <166118411+23DP1KIrbe@users.noreply.github.com>
2024-10-29 06:52:26 +0000

    Last chance gas

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -8,42 +8,91 @@ public class App
     public static void main( String[] args ) {
         Scanner scanner = new Scanner(System.in);
         
-        ArrayList<Integer> list = new ArrayList<>();
-        int number;
-        System.out.println("Enter numbers (enter -1 to break): ");
-        while(true){
+        // ArrayList<Integer> list = new ArrayList<>();
+        // int number;
+        // System.out.println("Enter numbers (enter -1 to break): ");
+        // while(true){
 
-            number = scanner.nextInt();
-            list.add(number);
-            if (number == -1) {
-                break;
+        //     number = scanner.nextInt();
+        //     list.add(number);
+        //     if (number == -1) {
+        //         break;
+        //     }
+        // }
+
+        // int nn = 0;
+        // int ff = 0;
+
+        // while(true){
+        //     System.out.println("From where" );
+        //     nn = scanner.nextInt();
+        //     if(nn < 0){
+        //         System.out.println("Not valid number");
+        //     }else{
+        //         break;
+        //     }
+        // }
+        // while(true){
+        //     System.out.println("To where: ");
+        //     ff = scanner.nextInt();
+        //     if(ff < list.size()){
+        //         System.out.println("Not valid number");
+        //     }else{
+        //         break;
+        //     }
+        // }
+        // for(int i = nn; i <= ff; i++){
+        //     System.out.println(list.get(i));
+        // }
+
+        
+        
+        
+        
+        Integer capa = Integer.valueOf(scanner.nextLine());
+        Integer gage = Integer.valueOf(scanner.nextLine());
+        Integer miles = Integer.valueOf(scanner.nextLine());
+        
+
+        System.out.println("Tank capacity:");
+        System.out.println(capa);
+        System.out.println("Gage reading:");
+        System.out.println(gage);
+        System.out.println("Miles per gallon:");
+        System.out.println(miles);
+
+        if(gage == 50 || gage == 100){
+            int extra = 0;
+            if(gage == 100){
+                extra = 1;
             }
-        }
-
-        int nn = 0;
-        int ff = 0;
-
-        while(true){
-            System.out.println("From where" );
-            nn = scanner.nextInt();
-            if(nn < 0){
-                System.out.println("Not valid number");
+            if(gage == 50){
+                extra = 2;
+            }
+            
+            int result = capa / extra;
+            if(result * miles > 200){
+                System.out.println("Safe to proceed");
             }else{
-                break;
+                System.out.println("Get gas!");
             }
         }
-        while(true){
-            System.out.println("To where: ");
-            ff = scanner.nextInt();
-            if(ff < list.size()){
-                System.out.println("Not valid number");
+        
+        if(gage == 75 || gage == 25){
+            int result2 = 0;
+            if(gage == 75){
+                result2 = capa / 4 * 3;
+            }
+            if(gage == 25){
+                result2 = capa / 4;
+            }
+            if(result2 * miles > 200){
+                System.out.println("Safe to proceed");
             }else{
-                break;
+                System.out.println("Get gas!");
             }
         }
-        for(int i = nn; i <= ff; i++){
-            System.out.println(list.get(i));
-        }
+        
         scanner.close();
     }
 }
